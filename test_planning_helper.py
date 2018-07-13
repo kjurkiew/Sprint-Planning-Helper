4d1279f1b00e29b85c7d334fe53f15b06c7029c4
Krzysztof <35111684+kjurkiew@users.noreply.github.com>
2018-07-13 17:23:48 +0200

    Changed tests to unittest

--- test_planning_helper.py
+++ test_planning_helper.py
@@ -1,40 +1,41 @@
-from sprint_planning_helper import *
+import sprint_planning_helper
+import unittest
 
-def test_collecting_data_from_csv():
-    total = collecting_data_from_csv('test.csv')
-    assert total[0] == [0, 1, 2, 3, 4, 5]
-    assert total[1] == {0: 3, 1: 2, 2: 8, 3: 5, 4: 2, 5: 2}
-    assert total[2] == {0: 5, 1: 3, 2: 2, 3: 9, 4: 1, 5: 3}
+class TestCalc(unittest.TestCase):
 
-def test_initialize_result_array():
-    total = initialize_result_array([0, 1, 2, 3, 4, 5], 13)
-    assert total == [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    def test_collecting_data_from_csv(self):
+        total = sprint_planning_helper.collecting_data_from_csv('test.csv')
+        self.assertEqual(total[0], [0, 1, 2, 3, 4, 5])
+        self.assertEqual(total[1], {0: 3, 1: 2, 2: 8, 3: 5, 4: 2, 5: 2})
+        self.assertEqual(total[2], {0: 5, 1: 3, 2: 2, 3: 9, 4: 1, 5: 3})
+
+    def test_initialize_result_array(self):
+        total = sprint_planning_helper.initialize_result_array([0, 1, 2, 3, 4, 5], 13)
+        self.assertEqual(total,
+                    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
-def test_knapsack():
-    array = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    data_from_csv = ([0, 1, 2, 3, 4, 5], {0: 3, 1: 2, 2: 8, 3: 5, 4: 2, 5: 2}, {0: 5, 1: 3, 2: 2, 3: 9, 4: 1, 5: 3})
+    def test_knapsack(self):
+        array = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        data_from_csv = ([0, 1, 2, 3, 4, 5], {0: 3, 1: 2, 2: 8, 3: 5, 4: 2, 5: 2}, {0: 5, 1: 3, 2: 2, 3: 9, 4: 1, 5: 3})
+        total = sprint_planning_helper.knapsack(array, data_from_csv, 13)
+        self.assertEqual(total, [5, 3, 1, 0])
 
-    total = knapsack(array,data_from_csv, 13)
-    assert total == [5, 3, 1, 0]
+    def test_conv_list_to_string(self):
+        total = sprint_planning_helper.conv_list_to_string([5, 3, 1, 0])
+        self.assertEqual(total, '0, 1, 3, 5')
 
-def test_conv_list_to_string():
-    total = conv_list_to_string([5, 3, 1, 0])
-    assert total == '0, 1, 3, 5'
-
+    
 if __name__ == '__main__':
-    test_collecting_data_from_csv()
-    test_initialize_result_array()
-    test_knapsack()
-    test_conv_list_to_string()
+    unittest.main()
