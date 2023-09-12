d1030498956aa605ce7bfa0c284797d7083f00ae
Krzysztof <35111684+kjurkiew@users.noreply.github.com>
2023-09-12 21:34:12 +0200

    Update sprint_planning_helper.py
    Type Hints: Added type hints for function parameters and return values, enhancing code readability and documentation.
    
    Constants for Column Names: Defined column names as constants (CSV_FILE_COLUMNS) at the script's start, improving code maintainability and ease of future updates.
    
    Error Handling: Implemented error handling to handle cases where the file is not found, enhancing code robustness.
    
    Improved Variable Names: Used more descriptive variable names (e.g., column), making the code easier to understand.
    
    Updated Argument Parsing: Enhanced argument parsing to verify the correct number of command-line arguments and provide a usage message when incorrect.
    
    Docstrings: Added docstrings to functions and test methods for clear documentation.

--- sprint_planning_helper.py
+++ sprint_planning_helper.py
@@ -1,26 +1,57 @@
-#! python 3
 # planning_helper.py - finding the most efficient set of tasks to be taken from the file
 
 import csv
-from sys import argv, stdout
+import sys
+from typing import List, Tuple, Dict
+
+CSV_FILE_COLUMNS = ["task_id", "story_points", "KSP"]
 
 
-def collecting_data_from_csv(file_name):
-    with open(file_name, 'r', encoding='utf-8') as csvfile:
-        data_from_csv = csv.DictReader(csvfile)
-        task_id = []
-        story_points = {}
-        KSP = {}
-        for row in data_from_csv:
-            task_id.append(int(row['task_id']))
-            story_points.update({int(row['task_id']): int(row['story_points'])})
-            KSP.update({int(row['task_id']): int(row['KSP'])})
+def collecting_data_from_csv(
+    file_name: str,
+) -> Tuple[List[int], Dict[int, int], Dict[int, int]]:
+    """
+    Collects data from a CSV file.
+
+    Args:
+        file_name (str): The name of the CSV file.
+
+    Returns:
+        Tuple[List[int], Dict[int, int], Dict[int, int]]: A tuple containing task IDs, story points, and KSP values.
+    """
+    task_id, story_points, KSP = [], {}, {}
+
+    try:
+        with open(file_name, "r", encoding="utf-8") as csvfile:
+            data_from_csv = csv.DictReader(csvfile)
+            for row in data_from_csv:
+                for column in CSV_FILE_COLUMNS:
+                    if column == "task_id":
+                        task_id.append(int(row[column]))
+                    else:
+                        column_data = int(row[column])
+                        if column == "story_points":
+                            story_points[int(row["task_id"])] = column_data
+                        elif column == "KSP":
+                            KSP[int(row["task_id"])] = column_data
+    except FileNotFoundError:
+        print("File not found.")
+        sys.exit(1)
 
     return task_id, story_points, KSP
 
 
-def initialize_result_array(task_id, velocity):
-    # result array for solving knapsack problem
+def initialize_result_array(task_id: List[int], velocity: int) -> List[List[int]]:
+    """
+    Initializes a result array for solving the knapsack problem.
+
+    Args:
+        task_id (List[int]): List of task IDs.
+        velocity (int): The available velocity.
+
+    Returns:
+        List[List[int]]: A 2D array for dynamic programming.
+    """
     result = []
     for i in range(len(task_id) + 1):
         result.append([0 for j in range(velocity + 1)])
@@ -28,10 +59,22 @@ def initialize_result_array(task_id, velocity):
     return result
 
 
-def knapsack(array, data_from_csv, velocity):
-    # A naive recursive implementation of 0-1 Knapsack Problem
-    # Normally this function returns the maximum value that can be put in a knapsack
-    # In this case function find most efficient set of tasks to be taken
+def knapsack(
+    array: List[List[int]],
+    data_from_csv: Tuple[List[int], Dict[int, int], Dict[int, int]],
+    velocity: int,
+) -> List[int]:
+    """
+    Solves the knapsack problem to find the most efficient set of tasks.
+
+    Args:
+        array (List[List[int]]): The result array for dynamic programming.
+        data_from_csv (Tuple[List[int], Dict[int, int], Dict[int, int]]): Data from the CSV file.
+        velocity (int): The available velocity.
+
+    Returns:
+        List[int]: A list of task IDs in the optimal solution.
+    """
     story_points = data_from_csv[1]
     KSP = data_from_csv[2]
     left_id = len(story_points)
@@ -44,14 +87,17 @@ def knapsack(array, data_from_csv, velocity):
                 array[ids][v] = 0
             # If value of the ids item is lower than left velocity
             # Return the maximum of two cases:
-            # 1 - element ids inculded
-            # 2 - element idsc not inculded
+            # 1 - element ids included
+            # 2 - element ids not included
             elif story_points[ids - 1] <= v:
-                array[ids][v] = max(KSP[ids - 1] + array[ids - 1][v - story_points[ids - 1]], array[ids - 1][v])
-            # If value of the element is higher then left velocity
-            # then this item cannot be included in the optimal soulution
+                array[ids][v] = max(
+                    KSP[ids - 1] + array[ids - 1][v - story_points[ids - 1]],
+                    array[ids - 1][v],
+                )
+            # If value of the element is higher than left velocity
+            # then this item cannot be included in the optimal solution
             else:
-                array[ids][v] = array[ids-1][v]
+                array[ids][v] = array[ids - 1][v]
 
     result = array[left_id][velocity]
 
@@ -62,24 +108,36 @@ def knapsack(array, data_from_csv, velocity):
         if result == array[ids - 1][v]:
             continue
         else:
-            output_tasks.append(ids-1)
-            result -= KSP[ids-1]
-            v -= story_points[ids-1]
+            output_tasks.append(ids - 1)
+            result -= KSP[ids - 1]
+            v -= story_points[ids - 1]
 
     return output_tasks
 
 
-def conv_list_to_string(output_tasks_list):
+def conv_list_to_string(output_tasks_list: List[int]) -> str:
+    """
+    Converts a list of task IDs to a comma-separated string.
+
+    Args:
+        output_tasks_list (List[int]): List of task IDs.
+
+    Returns:
+        str: Comma-separated string of task IDs.
+    """
     output_tasks_list = sorted(output_tasks_list)
-    output_tasks = ', '.join(map(str, output_tasks_list))
+    output_tasks = ", ".join(map(str, output_tasks_list))
 
     return output_tasks
 
 
-if __name__ == '__main__':
-    arguments = argv
-    file_name = arguments[1]
-    velocity = int(arguments[2])
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python planning_helper.py <file_name> <velocity>")
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+    velocity = int(sys.argv[2])
 
     data_from_csv = collecting_data_from_csv(file_name)
     task_id = data_from_csv[0]
@@ -87,4 +145,4 @@ if __name__ == '__main__':
     array = initialize_result_array(task_id, velocity)
     output_tasks_list = knapsack(array, data_from_csv, velocity)
     output_tasks = conv_list_to_string(output_tasks_list)
-    stdout.write(output_tasks)
+    sys.stdout.write(output_tasks)
